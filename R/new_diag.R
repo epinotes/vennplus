baba4bea753d26da506f1307120c03221c0c6826
Mamadou Ndiaye <epinotes@gmail.com>
2016-08-08 11:32:16 -0700

    Update new_diag.R

--- R/new_diag.R
+++ R/new_diag.R
@@ -1,9 +1,9 @@
-function(expr,colvec,dat){
+function(data, expr,colvec,ignore.case = T){
   #regexp=regular expressions for the data
   #a vector of the columns of interest (columns with the diagnsotics
   c1<-length(colvec)
-  mat1<-as.matrix(dat[,colvec])
-  mat2<-unlist((grepl(expr,mat1)+0),byrow=F,ncol=c1)
+  mat1<-unlist(data[,colvec])
+  mat2<-matrix((grepl(expr,mat1, ignore.case = ignore.case)+0),byrow=F,ncol=c1)
   #addiding "0" transform the logical into 1 for TRUE and 0 for FALSE
   tab.sum<-apply(mat2,MARGIN=1,FUN=sum)
   tab.sum[tab.sum!=0]<-1
